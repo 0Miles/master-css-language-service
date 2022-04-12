453e2289ceefd9b82b26d728ddf47a1c528e5cb5
Bern <doge427538@gmail.com>
2022-04-12 21:36:08 +0800

    自動完成錯誤修正 上色修正

--- server/src/server.ts
+++ server/src/server.ts
@@ -382,7 +382,12 @@ connection.onCompletion(
     }
 );
 
+connection.onCompletionResolve(
+    (item: CompletionItem): CompletionItem => {
 
+        return item;
+    }
+);
 // Make the text document manager listen on the connection
 // for open, change and close text document events
 documents.listen(connection);
