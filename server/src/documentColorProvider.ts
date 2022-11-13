d955a6a45aea36d8c41efb32f0564d5df6fdf337
Bern <doge427538@gmail.com>
2022-11-13 18:39:59 +0800

    fix: color preview

--- server/src/documentColorProvider.ts
+++ server/src/documentColorProvider.ts
@@ -65,7 +65,7 @@ export async function GetDocumentColors(DocumentColor: DocumentColorParams, docu
         const instanceStartIndex = instanceMatch.index;
         const theme = masterCss.themeNames.find(x => instanceMatch?.[0]?.endsWith(`@${x}`)) ?? '';
 
-        const colorPattern = /(?<=[:|])(?:#?\w+(?:-[\d]{1,2})?(?:\/.?[\d]*)?(?:\([^\s\)]+\))?)/g;
+        const colorPattern = /(?<=[:;|])(?:#?\w+(?:-[\d]{1,2})?(?:\/.?[\d]*)?(?:\([^\s\)]+\))?)/g;
         const rgbaColorPattern = /rgba?\(([\d.]+),([\d.]+),([\d.]+)(?:,([\d.]+))?\)/g;
         const hslaColorPattern = /hsla?\(([\d.]+),([\d.]+)%,([\d.]+)%(?:,([\d.]+))?\)/g;
         const hexColorPattern = /#([0-9a-fA-F]{6,8})/g;
